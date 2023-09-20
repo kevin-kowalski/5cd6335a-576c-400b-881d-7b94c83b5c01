489a1a2298aad01ebc6572c578f6e089a0a4466a
Kevin Kowalski <hello@kevinkowalski.com>
2023-09-20 20:58:27 +0200

    feat: add Header with Search and CartButton components

---
+++ client/src/components/Header.tsx
@@ -0,0 +1,12 @@
+import CartButton from "./CartButton";
+import Search from "./Search";
+
+export default function Header () {
+
+  return (
+    <header>
+      <Search />
+      <CartButton />
+    </header>
+  );
+}
